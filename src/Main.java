b32fd99ffe03facb6129e93e9c02bb980f798470
Usuario <luis40105@edu.unipar.edu>
2024-09-04 20:27:45 -0300

    inicial aula 4

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+
+                            int votopessoa1 = 10;
+                            int votopessoa2 = 23;
+                            int votopessoa3 = 10;
+                            int votopessoa4 = 10;
+                            int votopessoa5 = 99;
+                            int votopessoa6 = 10;
+                            int votopessoa7 = 99;
+                            int votopessoa8 = 10;
+                            int votopessoa9 = 23;
+                            int votopessoa10 = 99;
+
+                            // esse começa do 0 ao 10, ou seja é o mesmo codigo acima porém em linha menor.
+                            // porém é um vetor vazio.
+                            int[] votos = new int[10];
+
+                            // nomeVETOR[POSIÇÃO] = VETOR.
+                            // aqui começa a colocar valores dentro do vetor.
+                            votos[0] = 90;
+                            votos[1] = 10;
+                            votos[2] = 20;
+
+                            String[] nomes = new String[5];
+                            Scanner entrada = new Scanner(System.in);
+                            nomes[0] = entrada.next();
+                            System.out.println("o primeiro nome é :" + nomes[0]);
+
+                            double[] valores = {5.5,10.9,99,75};
+
+
+
+                    }
+
+
+}
